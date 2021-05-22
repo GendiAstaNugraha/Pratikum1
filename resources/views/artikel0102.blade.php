41321727e6ac7c20d6b9918eedd164a79a6fe4a4
GendiAstaNugraha <astagendi@gmail.com>
2021-05-22 14:20:07 +0700

    default

---
+++ resources/views/artikel0102.blade.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <nav class="nav">
+  <a class="nav-link" href="{{url('/home')}}">Home</a>
+  <a class="nav-link" href="{{url('/artikel')}}">Artikel</a>
+  <a class="nav-link" href="{{url('/contact')}}">Contact us</a>
+</nav>
+<div class="container">
+    <h1>Yuk! Mengenal Apa Itu Framework & Fungsinya</h6>
+    <h6>Anda mungkin sedang bertanya: “Apa itu Framework?” Nah, framework adalah sebuah kerangka program yang digunakan untuk membantu developer untuk mengembangkan kode secara konsisten. 
+
+Dengan adanya framework developer bisa mengurangi jumlah bug pada website atau aplikasi yang dibuat. Karena, fungsi dan variabel yang sudah tersedia di dalam komponen framework.
+
+Dalam artikel ini, Anda akan mempelajari lebih dalam pengertian dan fungsi framework. Tak hanya itu, Anda pun akan diajak mengenal jenis jenis framework yang sering digunakan. Yuk, simak artikel ini sampai selesai.</h4>
+<h6>Apa Itu Framework?
+Framework adalah kerangka kerja yang digunakan untuk mengembangkan aplikasi berbasis desktop atau aplikasi berbasis website. Dengan menggunakan framework Anda akan lebih mudah untuk membuat aplikasi atau website. Itu karena Anda hanya perlu menyusun komponen-komponen pemrograman yang sudah jadi. Dengan kata lain, Anda tidak perlu membuat berbagai fitur dari awal lagi.
+
+Framework sengaja diciptakan untuk membantu developer mengembangkan aplikasi atau website lebih cepat serta tersusun dan terstruktur. 
+
+Dengan begitu, developer dan programmer tidak perlu melakukan koding program yang diulang-ulang.
+
+Nah, sudah tahu kan apa itu framework? Sekarang, mari pelajari fungsinya.
+
+Fungsi Framework
+Framework memiliki fungsi utama untuk mempermudah para developer mengembangkan aplikasi dan website terkait struktur MVC (Model View Controller) yang digunakan. Selain itu, ada tiga fungsi framework yang dapat Anda ketahui di bawah ini:
+</h6>
+<h4> 1. Program Menjadi Lebih Terstruktur dan Tersusun</h4>
+Saat developer mengerjakan pengembangan aplikasi atau website yang besar, maka program yang akan ditulis di dalamnya menjadi semakin banyak. Terkadang, semakin banyaknya program yang ditulis akan menjadikan proses debugging semakin lambat. Selain itu, saat Anda mencari kode program yang error akan semakin sulit karena program yang tidak terstruktur.
+
+Oleh karena itu, dengan adanya framework dapat menjadikan program menjadi lebih terstruktur sehingga Anda dapat dengan mudah menemukan kode yang perlu diperbaiki. Bahkan beberapa jenis framework ada yang menerapkan konsep MVC (Model View Controller) yang akan memudahkan developer untuk memisahkan antara logika dan view.
+
+<h4> 2. Praktis untuk Developer</h4>
+Saat Anda mengembangkan aplikasi atau website, akan lebih praktis ketika Anda menggunakan framework. Alasannya karena framework sudah menyediakan kode berupa function dan class jadi proses pembuatan software atau aplikasi akan menjadi lebih cepat. Anda cukup memanggil function atau class tersebut ke dalam kode program Anda.
+
+
+<h4> 3. Memiliki Keamanan yang Lebih Unggul</h4>
+Selama bertahun-tahun, keamanan untuk aplikasi dan website telah menjadi perhatian utama bagi developer. Beberapa dari developer telah menggunakan segala cara untuk  mengamankan aplikasi dan website yang telah mereka buat.
+
+Setelah framework hadir, developer boleh sedikit tenang soal keamanan program yang telah dibuat. Karena setiap celah keamanan dari framework sudah diidentifikasi oleh 10-100+ developer ahli. Framework juga terus diperbarui versinya untuk menawarkan fitur baru dan menangani bug yang meminimalisir celah keamanan framework.</h6>
+</div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
